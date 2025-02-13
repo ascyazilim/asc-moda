965d537d462d342c2609da42f62e19ae2642f130
ascyazilim <yazilimasc@gmail.com>
2025-02-13 16:22:45 +0300

    feat(auth): Added UserDetailsService to load user details from database for Spring Security

---
+++ user-service/src/main/java/com/asc/userservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.asc.userservice.security;
+
+import com.asc.userservice.entity.User;
+import com.asc.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
