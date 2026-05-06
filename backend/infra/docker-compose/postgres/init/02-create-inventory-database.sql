637475b5e50d308406a556e08a2103a36b158353
ascyazilim <yazilimasc@gmail.com>
2026-05-06 20:25:21 +0300

    infra: postgres init scriptleri düzeltildi
    - Inventory ve cart veritabanı oluşturma scriptlerindeki hata giderildi.
    
    - Docker compose altında lokal PostgreSQL başlatma akışı daha tutarlı hale getirildi.

--- backend/infra/docker-compose/postgres/init/02-create-inventory-database.sql
+++ backend/infra/docker-compose/postgres/init/02-create-inventory-database.sql
@@ -1,1 +1,4 @@
-CREATE DATABASE asc_moda_inventory;
+SELECT 'CREATE DATABASE asc_moda_inventory'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'asc_moda_inventory'
+)\gexec
--- backend/infra/docker-compose/postgres/init/03-create-cart-database.sql
+++ backend/infra/docker-compose/postgres/init/03-create-cart-database.sql
@@ -1,1 +1,4 @@
-CREATE DATABASE asc_moda_cart;
+SELECT 'CREATE DATABASE asc_moda_cart'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'asc_moda_cart'
+)\gexec
